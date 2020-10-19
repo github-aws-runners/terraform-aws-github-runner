c21d2e6bd01dcb50b1096283e7b00f839830e26b
Simon Jagoe <simon.jagoe@smartly.io>
2020-10-19 15:39:08 +0300

    Fix terraform format

--- examples/ubuntu/main.tf
+++ examples/ubuntu/main.tf
@@ -30,6 +30,7 @@ module "runners" {
   webhook_lambda_zip                = "lambdas-download/webhook.zip"
   runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
   runners_lambda_zip                = "lambdas-download/runners.zip"
+
   enable_organization_runners = false
   runner_extra_labels         = "ubuntu,example"
 
