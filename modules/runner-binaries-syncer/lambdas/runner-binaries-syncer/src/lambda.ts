34533418d236a615f10651539e1c3ab2d89ffcb9
github-auto2 <guoluome@hotmail.com>
2020-11-30 20:40:15 +0800

    support windows binnaries

--- modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/lambda.ts
+++ modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/lambda.ts
@@ -1,7 +1,9 @@
 import { handle } from './syncer/handler';
+import { handleforwin } from './syncer/handlerforWin';
 
 // eslint-disable-next-line
 module.exports.handler = async (event: any, context: any, callback: any): Promise<any> => {
   await handle();
+  await handleforwin();
   return callback();
 };
