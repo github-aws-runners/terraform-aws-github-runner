9413ac9692a7c3d7e54456022162afc9ac23c255
github-auto2 <guoluome@hotmail.com>
2020-11-30 20:42:29 +0800

    support windows binaries

--- modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/syncer/handler.ts
+++ modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/syncer/handler.ts
@@ -12,9 +12,6 @@ interface CacheObject {
   key: string;
 }
 
-
-
-
 async function getCachedVersion(s3: S3, cacheObject: CacheObject): Promise<string | undefined> {
   try {
     const objectTagging = await s3
@@ -30,26 +27,6 @@ async function getCachedVersion(s3: S3, cacheObject: CacheObject): Promise<strin
     return undefined;
   }
 }
-
-
-
-async function getCachedVersionwindows(s3: S3, cacheObjectwindows: CacheObject): Promise<string | undefined> {
-  try {
-    const objectTaggingwindows = await s3
-      .getObjectTaggingwindows({
-        Bucket: cacheObjectwindows.bucket,
-        Key: cacheObjectwindows.key,
-      })
-      .promise();
-    const versionswindows = objectTaggingwindows.TagSet?.filter((t: S3.Tag) => t.Key === versionKey);
-    return versions.length === 1 ? versions[0].Value : undefined;
-  } catch (e) {
-    console.debug('No tags found');
-    return undefined;
-  }
-}
-
-
 interface ReleaseAsset {
   name: string;
   downloadUrl: string;
@@ -80,44 +57,11 @@ async function getLinuxReleaseAsset(
     return undefined;
   }
   const linuxAssets = asset.assets?.filter((a) => a.name?.includes(`actions-runner-linux-${runnerArch}-`));
-  
+
   return linuxAssets?.length === 1
     ? { name: linuxAssets[0].name, downloadUrl: linuxAssets[0].browser_download_url }
     : undefined;
 }
-
-
-async function getWindowsReleaseAsset(
-  runnerArch = 'x64',
-  fetchPrereleaseBinaries = false,
-): Promise<ReleaseAsset | undefined> {
-  const githubClient = new Octokit();
-  const assetsList = await githubClient.repos.listReleases({
-    owner: 'actions',
-    repo: 'runner',
-  });
-  if (assetsList.data?.length === 0) {
-    return undefined;
-  }
-
-  const latestPrereleaseIndex = assetsList.data.findIndex((a) => a.prerelease === true);
-  const latestReleaseIndex = assetsList.data.findIndex((a) => a.prerelease === false);
-
-  let asset = undefined;
-  if (fetchPrereleaseBinaries && latestPrereleaseIndex < latestReleaseIndex) {
-    asset = assetsList.data[latestPrereleaseIndex];
-  } else if (latestReleaseIndex != -1) {
-    asset = assetsList.data[latestReleaseIndex];
-  } else {
-    return undefined;
-  }
-
-  const windowsAssets = asset.assets?.filter((a) => a.name?.includes(`actions-runner-win-${runnerArch}-`));
-  return windowsAssets?.length === 1
-    ? { name: windowsAssets[0].name, downloadUrl: windowsAssets[0].browser_download_url }
-    : undefined;
-}
-
 
 async function uploadToS3(s3: S3, cacheObject: CacheObject, actionRunnerReleaseAsset: ReleaseAsset): Promise<void> {
   const writeStream = new PassThrough();
@@ -134,7 +78,7 @@ async function uploadToS3(s3: S3, cacheObject: CacheObject, actionRunnerReleaseA
       .get(actionRunnerReleaseAsset.downloadUrl)
       .pipe(writeStream)
       .on('finish', () => {
-        console.info(`The new Linux distribution is uploaded to S3.`);
+        console.info(`The new distribution is uploaded to S3.`);
         resolve();
       })
       .on('error', (error) => {
@@ -144,38 +88,6 @@ async function uploadToS3(s3: S3, cacheObject: CacheObject, actionRunnerReleaseA
     console.error(`Exception: ${error}`);
   });
 }
-
-
-
-async function uploadToS3windows(s3: S3, cacheObjectwindows: CacheObject, actionRunnerReleaseAssetwindows: ReleaseAsset): Promise<void> {
-  const writeStreamwindows = new PassThrough();
-  s3.upload({
-    Bucket: cacheObjectwindows.bucket,
-    Key: cacheObjectwindows.key,
-    Tagging: versionKey + '=' + actionRunnerReleaseAssetwindows.name,
-    Body: writeStreamwindows,
-  }).promise();
-
-  await new Promise<void>((resolve, reject) => {
-    console.debug('Start downloading %s and uploading to S3.', actionRunnerReleaseAssetwindows.name);
-    request
-      .get(actionRunnerReleaseAssetwindows.downloadUrl)
-      .pipe(writeStreamwindows)
-      .on('finish', () => {
-        console.info(`The new Windows distribution is uploaded to S3.`);
-        resolve();
-      })
-      .on('error', (error) => {
-        reject(error);
-      });
-  }).catch((error) => {
-    console.error(`Exception: ${error}`);
-  });
-}
-
-
-
-
 
 export const handle = async (): Promise<void> => {
   const s3 = new AWS.S3();
@@ -191,43 +103,16 @@ export const handle = async (): Promise<void> => {
     throw Error('Please check all mandatory variables are set.');
   }
 
-
-const cacheObjectwindows: CacheObject = {
-    bucket: process.env.S3_BUCKET_NAME as string,
-    key: process.env.S3_OBJECT_KEY as string,
-  };
-  if (!cacheObjectwindows.bucket || !cacheObjectwidnows.key) {
-    throw Error('Please check all mandatory variables are set.');
-  }
-
-
-
   const actionRunnerReleaseAsset = await getLinuxReleaseAsset(runnerArch, fetchPrereleaseBinaries);
-
   if (actionRunnerReleaseAsset === undefined) {
     throw Error('Cannot find GitHub release asset.');
   }
- 
+
   const currentVersion = await getCachedVersion(s3, cacheObject);
   console.debug('latest: ' + currentVersion);
   if (currentVersion === undefined || currentVersion != actionRunnerReleaseAsset.name) {
     uploadToS3(s3, cacheObject, actionRunnerReleaseAsset);
   } else {
-    console.debug('Linux distribution is up-to-date, no action.');
+    console.debug('Distribution is up-to-date, no action.');
   }
-
- 
-
- const actionRunnerReleaseAssetwindows = await getWindowsReleaseAsset(runnerArch, fetchPrereleaseBinaries);
-  if (actionRunnerReleaseAssetwindows === undefined) {
-    throw Error('Cannot find GitHub release asset.');
-  }
- const currentVersionwindows = await getCachedVersionwindows(s3, cacheObjectwindows);
-  console.debug('latest: ' + currentVersionwindows);
-  if (currentVersionwindows === undefined || currentVersionwindows != actionRunnerReleaseAssetwindows.name) {
-    uploadToS3(s3, cacheObjectwindows, actionRunnerReleaseAssetwindows);
-  } else {
-    console.debug('Windows distribution is up-to-date, no action.');
-  }
-
 };
