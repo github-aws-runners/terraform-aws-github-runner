aa71cce2022241f87334d6fc6dbc0b90b024afcb
Niek Palm <dev.npalm@gmail.com>
2020-12-07 14:17:01 +0100

    Update docs

--- modules/runners/variables.tf
+++ modules/runners/variables.tf
@@ -246,13 +246,13 @@ variable "runner_iam_role_managed_policy_arns" {
 }
 
 variable "enable_cloudwatch_agent" {
-  description = "Enabling the cloudwatch agent on the ec2 runner instances, the runner contains default config. Configuration can be overrided via `cloudwatch_config`."
+  description = "Enabling the cloudwatch agent on the ec2 runner instances, the runner contains default config. Configuration can be overridden via `cloudwatch_config`."
   type        = bool
   default     = true
 }
 
 variable "cloudwatch_config" {
-  description = "(optional) to replace the module default cloudwatch log config. See https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Agent-Configuration-File-Details.html for details."
+  description = "(optional) Replaces the module default cloudwatch log config. See https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Agent-Configuration-File-Details.html for details."
   type        = string
   default     = null
 }
