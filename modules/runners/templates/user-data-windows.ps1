a7f732c6783ea4e2ac5c55b2c7945bbaa24ea6e8
github-auto2 <guoluome@hotmail.com>
2020-12-07 18:16:51 +0800

    remove someting

--- modules/runners/templates/user-data-windows.ps1
+++ modules/runners/templates/user-data-windows.ps1
@@ -1,5 +1,5 @@
 mkdir actions-runner; cd actions-runner
 Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.272.0/actions-runner-win-x64-2.272.0.zip -OutFile actions-runner-win-x64-2.272.0.zip 
 Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.272.0.zip", "$PWD")
-./config.cmd --url https://github-reh.azc.ext.hp.com/grouptestactionautoscaling --token AAAADREQF3GZEAX7OH7U3327ZW3NQ
+./config.cmd --url https://github.hp.com/grouptestactionautoscaling --token XXXXX
 ./run.cmd
