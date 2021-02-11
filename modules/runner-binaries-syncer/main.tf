7b8fec7f8bcd4301585aad83774074517c44c684
Nathaniel McAuliffe <nmcauliffe@expedia.com>
2021-02-11 10:20:19 -0500

    fix(bucket): Adds bucket policy - Block public access - Set transition to intelligent tiering - Deletes failed uploads

--- modules/runner-binaries-syncer/main.tf
+++ modules/runner-binaries-syncer/main.tf
@@ -7,4 +7,21 @@ resource "aws_s3_bucket" "action_dist" {
   acl           = "private"
   force_destroy = true
   tags          = var.tags
+
+  lifecycle {
+    abort_incomplete_multipart_upload_days = 7
+
+    transition {
+      days          = 35
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+}
+
+resource "aws_s3_buckt_public_access_block" "action_dist" {
+  bucket                  = aws_s3_bucket.action_dist.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
