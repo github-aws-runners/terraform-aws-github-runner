df506f7e56da7a42c6973bc6cac63981de9ff060
Callum <callum@domain.com>
2021-11-02 18:22:05 +0000

    chore: fmt ts files

--- modules/runners/lambdas/runners/src/scale-runners/gh-auth.ts
+++ modules/runners/lambdas/runners/src/scale-runners/gh-auth.ts
@@ -48,10 +48,12 @@ async function createAuth(installationId: number | undefined, ghesApiUrl: string
     privateKey: Buffer.from(
       await getParameterValue(process.env.PARAMETER_GITHUB_APP_KEY_BASE64_NAME),
       'base64',
-    // replace literal \n characters with new lines to allow the key to be stored as a 
-    // single line variable. This logic should match how the GitHub Terraform provider 
-    // processes private keys to retain compatibility between the projects
-    ).toString().replace('/[\\n]/g', String.fromCharCode(13)),
+      // replace literal \n characters with new lines to allow the key to be stored as a
+      // single line variable. This logic should match how the GitHub Terraform provider
+      // processes private keys to retain compatibility between the projects
+    )
+      .toString()
+      .replace('/[\\n]/g', String.fromCharCode(13)),
   };
   if (installationId) authOptions = { ...authOptions, installationId };
 
