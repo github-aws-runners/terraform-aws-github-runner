5f319f0b82f007bca06d8e7e51e2f8902f69c66d
Nathaniel McAuliffe <nmcauliffe@expediagroup.com>
2021-11-09 08:32:56 -0500

    Remove testing
    Signed-off-by: Nathaniel McAuliffe <nmcauliffe@expediagroup.com>

--- modules/runners/lambdas/runners/src/scale-runners/logger.ts
+++ modules/runners/lambdas/runners/src/scale-runners/logger.ts
@@ -9,31 +9,6 @@ export const logger = new Logger({
   overwriteConsole: true,
   type: process.env.LOG_TYPE || 'pretty',
 });
-
-//interface LogFields {
-//  [key: string]: string;
-//}
-
 export class LogFields {
   static fields: { [key: string]: string } = {};
 }
-
-// Should I overload each method...? This would allow for always adding fields
-
-/*export class AwesomeLogger {
-  //logger: Logger;
-  params: ISettingsParam = {
-    colorizePrettyLogs: false,
-    displayInstanceName: false,
-    maskAnyRegEx: ['--token [A-Z0-9]*'],
-    minLevel: process.env.LOG_LEVEL || 'info',
-    overwriteConsole: true,
-    type: process.env.LOG_TYPE || 'pretty',
-  };
-
-  static fields: LogFields = {};
-
-  constructor(name: string) {
-    new Logger({ ...this.params, name: name });
-  }
-}*/
