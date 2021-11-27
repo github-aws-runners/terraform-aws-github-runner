9a4bb2fb2fd796dca812ab38d04d109b674d5ce1
Scott Guymer <scott.guymer@philips.com>
2021-11-27 15:39:23 +0100

    Retrieve external config setting via tags
    Retrieve the required config via the instance tags so we dont have to pass in and set environment on the instance in an awkward way.

--- images/startup.sh
+++ images/startup.sh
@@ -1,47 +1,49 @@
 #!/bin/bash -e
+exec > >(tee /var/log/runner-startup.log | logger -t user-data -s 2>/dev/console) 2>&1
 
 user_name=ec2-user
-
-if [[ -z "$ENVIRONMENT" ]]; then
-  echo "ENVIRONMENT is not set"
-  exit 1
-fi
-
-if [[ -n "$ENABLE_CLOUDWATCH_AGENT" ]]; then
-  if [[ -z "$SSM_KEY_CLOUDWATCH_AGENT_CONFIG" ]]; then
-    echo "Cloudwatch is enabled with ENABLE_CLOUDWATCH_AGENT but SSM_KEY_CLOUDWATCH_AGENT_CONFIG is not set"
-    exit 1
-  fi
-  echo "Cloudwatch is enabled"
-  amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:"$SSM_KEY_CLOUDWATCH_AGENT_CONFIG"
-fi
 
 cd /home/$user_name/actions-runner
 
 echo "Retrieving TOKEN from AWS API"
 token=$(curl -f -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 180")
 
-echo "Retrieving REGION from AWS API"
 region=$(curl -f -H "X-aws-ec2-metadata-token: $token" -v http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+echo "Reteieved REGION from AWS API ($region)"
 
-echo "Retrieving INSTANCE_ID from AWS API"
 instance_id=$(curl -f -H "X-aws-ec2-metadata-token: $token" -v http://169.254.169.254/latest/meta-data/instance-id)
+echo "Reteieved INSTANCE_ID from AWS API ($instance_id)"
+
+tags=$(aws ec2 describe-tags --region "$region" --filters "Name=resource-id,Values=$instance_id")
+echo "Retrieved tags from AWS API ($tags)"
+
+environment=$(echo "$tags" | jq '.Tags[]  | select(.Key == "Environment") | .Value')
+echo "Reteieved environment tag - ($environment)"
+
+enable_cloudwatch_agent=$(echo "$tags" | jq '.Tags[]  | select(.Key == "enable_cloudwatch_agent") | .Value')
+echo "Reteieved enable_cloudwatch_agent tag - ($enable_cloudwatch_agent)"
+
+if [[ -n "$enable_cloudwatch_agent" ]]; then  
+  echo "Cloudwatch is enabled"  
+  amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:"$environment-cloudwatch_agent_config_runner"
+fi
 
 echo "Get GH Runner token from AWS SSM"
-config=$(aws ssm get-parameters --names "$ENVIRONMENT"-"$instance_id" --with-decryption --region "$region" | jq -r ".Parameters | .[0] | .Value")
+config=$(aws ssm get-parameters --names "$environment"-"$instance_id" --with-decryption --region "$region" | jq -r ".Parameters | .[0] | .Value")
 
 while [[ -z "$config" ]]; do
   echo "Waiting for GH Runner token to become available in AWS SSM"
   sleep 1
-  config=$(aws ssm get-parameters --names "$ENVIRONMENT"-"$instance_id" --with-decryption --region "$region" | jq -r ".Parameters | .[0] | .Value")
+  config=$(aws ssm get-parameters --names "$environment"-"$instance_id" --with-decryption --region "$region" | jq -r ".Parameters | .[0] | .Value")
 done
 
 echo "Delete GH Runner token from AWS SSM"
-aws ssm delete-parameter --name "$ENVIRONMENT"-"$instance_id" --region "$region"
+aws ssm delete-parameter --name "$environment"-"$instance_id" --region "$region"
 
 echo "Configure GH Runner"
 ./config.sh --unattended --name "$instance_id" --work "_work" "$config"
 
+# TODO this should also be passed via tags
 service_user=${RUN_AGENT_AS_USER:-$user_name}
 
 echo "Start the runner as user $service_user"
