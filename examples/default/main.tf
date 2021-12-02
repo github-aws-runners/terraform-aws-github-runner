febcad911a999aa5daaa05ac2b89b1bdfe8541c2
Scott Guymer <scott.guymer@philips.com>
2021-12-02 17:40:43 +0100

    Reset default example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -13,14 +13,11 @@ resource "random_password" "random" {
 ################################################################################
 
 module "runners" {
-  source = "../../"
-
-  aws_region = local.aws_region
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-
+  source                          = "../../"
   create_service_linked_role_spot = true
+  aws_region                      = local.aws_region
+  vpc_id                          = module.vpc.vpc_id
+  subnet_ids                      = module.vpc.private_subnets
 
   environment = local.environment
   tags = {
@@ -33,18 +30,11 @@ module "runners" {
     webhook_secret = random_password.random.result
   }
 
-  webhook_lambda_zip                = "../../lambda_output/webhook.zip"
-  runner_binaries_syncer_lambda_zip = "../../lambda_output/runner-binaries-syncer.zip"
-  runners_lambda_zip                = "../../lambda_output/runners.zip"
-  enable_organization_runners       = true
-
-  runner_group_name   = "scottguymer"
-  runner_extra_labels = "default,example"
-
-
-  # userdata_template = "empty.sh"
-  # ami_filter        = { name = ["github-runner-amzn2-2021*"] }
-  # ami_owners        = ["132592213184"]
+  webhook_lambda_zip                = "lambdas-download/webhook.zip"
+  runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
+  runners_lambda_zip                = "lambdas-download/runners.zip"
+  enable_organization_runners       = false
+  runner_extra_labels               = "default,example"
 
   # enable access to the runners via SSM
   enable_ssm_on_runners = true
