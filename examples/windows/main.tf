61f0cafa35589a45b8b438f13dc5073ed98e1078
Scott Guymer <scott.guymer@philips.com>
2021-12-02 14:14:09 +0100

    Added more documentation to the windows example

--- examples/windows/main.tf
+++ examples/windows/main.tf
@@ -10,15 +10,10 @@ resource "random_password" "random" {
 module "runners" {
   source = "../../"
 
-  aws_region = local.aws_region
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  runner_os   = "win"
+  aws_region  = local.aws_region
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
   environment = local.environment
-  tags = {
-    Project = "ProjectX"
-  }
 
   github_app = {
     key_base64     = var.github_app_key_base64
@@ -26,41 +21,27 @@ module "runners" {
     webhook_secret = random_password.random.result
   }
 
+  # Grab the lambda packages from local directory. Must run /.ci/build.sh first
   webhook_lambda_zip                = "../../lambda_output/webhook.zip"
   runner_binaries_syncer_lambda_zip = "../../lambda_output/runner-binaries-syncer.zip"
   runners_lambda_zip                = "../../lambda_output/runners.zip"
-  enable_organization_runners       = true
 
+  enable_organization_runners = true
   runner_extra_labels         = "default,example"
+
+  # Set the OS to Windows
+  runner_os = "win"
+  # no need to add extra windows tag here as it is automatically added by GitHub
   runner_boot_time_in_minutes = 20
 
   # enable access to the runners via SSM
   enable_ssm_on_runners = true
 
-  ami_filter = {
-    name = ["Windows_Server-20H2-English-Core-ContainersLatest-*"]
-  }
-
-  runner_log_files = [
-    {
-      "log_group_name" : "user_data",
-      "prefix_log_group" : true,
-      "file_path" : "C:/UserData.log",
-      "log_stream_name" : "{instance_id}"
-    },
-    {
-      "log_group_name" : "runner",
-      "prefix_log_group" : true,
-      "file_path" : "C:/actions-runner/_diag/Runner_**.log",
-      "log_stream_name" : "{instance_id}"
-    }
-  ]
-
   instance_types = ["m5.large", "c5.large"]
 
-  # override delay of events in seconds
+  # override delay of events in seconds for testing
   delay_webhook_event = 5
 
-  # override scaling down
+  # override scaling down for testing
   scale_down_schedule_expression = "cron(* * * * ? *)"
 }
