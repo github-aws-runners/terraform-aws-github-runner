33ea24b81aa707a4d520fcff52ddfac258c0dd94
Scott Guymer <scott.guymer@philips.com>
2021-12-02 17:47:07 +0100

    Improve readability

--- modules/runners/scale-down.tf
+++ modules/runners/scale-down.tf
@@ -1,3 +1,10 @@
+locals {
+  # Windows Runners can take their sweet time to do anything
+  min_runtime_defaults = {
+    "win"   = 15
+    "linux" = 5
+  }
+}
 resource "aws_lambda_function" "scale_down" {
   s3_bucket         = var.lambda_s3_bucket != null ? var.lambda_s3_bucket : null
   s3_key            = var.runners_lambda_s3_key != null ? var.runners_lambda_s3_key : null
@@ -18,10 +25,7 @@ resource "aws_lambda_function" "scale_down" {
       GHES_URL                             = var.ghes_url
       LOG_LEVEL                            = var.log_level
       LOG_TYPE                             = var.log_type
-      MINIMUM_RUNNING_TIME_IN_MINUTES = (
-        # Windows Runners can take their sweet time to do anything
-        var.minimum_running_time_in_minutes != null ? var.minimum_running_time_in_minutes : var.runner_os == "linux" ? 5 : 15
-      )
+      MINIMUM_RUNNING_TIME_IN_MINUTES      = coalesce(var.minimum_running_time_in_minutes, min_runtime_defaults[var.runner_os])
       NODE_TLS_REJECT_UNAUTHORIZED         = var.ghes_url != null && !var.ghes_ssl_verify ? 0 : 1
       PARAMETER_GITHUB_APP_ID_NAME         = var.github_app_parameters.id.name
       PARAMETER_GITHUB_APP_KEY_BASE64_NAME = var.github_app_parameters.key_base64.name
