21bd3506fec5047b77c4d46bd11acca5b0cdac1a
Niek Palm <dev.npalm@gmail.com>
2021-12-07 11:06:23 +0100

    Add missing await

--- modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/lambda.ts
+++ modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/src/lambda.ts
@@ -2,15 +2,13 @@ import { sync } from './syncer/syncer';
 import { logger } from './syncer/logger';
 
 // eslint-disable-next-line
-export async function handler(event: any, context: any, callback: any): Promise<void> {
+export async function handler(event: any, context: any): Promise<void> {
   logger.setSettings({ requestId: context.awsRequestId });
   logger.debug(JSON.stringify(event));
 
   try {
     await sync();
-    callback(null);
   } catch (e) {
     logger.warn('Ignoring error:', e);
-    callback(e);
   }
 }
