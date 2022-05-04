4e803b296347e8844d53028bd969fa98994b078c
Niek Palm <niek.palm@philips.com>
2022-05-04 23:13:55 +0200

    fix: Volume size is ingored
    The problen is most likely introduces in one of the PR's to support multiple ebs.
    
    The following options for volumes are now supported.
    - Default, create an ebs for amazon linux
    - Provide explicit ebs mappings via block_device_mappings
    - No ebs, set block_device_mappings to []
    - Decrecated! Overwrite the default volume size by setting volume_size
    
    close: #1954

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -29,13 +29,15 @@ module "runners" {
     id             = var.github_app_id
     webhook_secret = random_id.random.hex
   }
+  
+  block_device_mappings = []
 
   # Grab zip files via lambda_download
-  webhook_lambda_zip                = "lambdas-download/webhook.zip"
-  runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
-  runners_lambda_zip                = "lambdas-download/runners.zip"
+  # webhook_lambda_zip                = "lambdas-download/webhook.zip"
+  # runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
+  # runners_lambda_zip                = "lambdas-download/runners.zip"
 
-  enable_organization_runners = false
+  enable_organization_runners = true
   runner_extra_labels         = "default,example"
 
   # enable access to the runners via SSM
