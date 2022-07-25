d5a51bf9f3cfa56529c704a6f3f21fef08a22dd0
GuptaNavdeep1983 <navdeep.gupta@philips.com>
2022-07-25 11:05:47 -0400

    fixed formatting

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -13,7 +13,7 @@ resource "random_id" "random" {
 ################################################################################
 
 module "runners" {
-  source = "../../"
+  source                          = "../../"
   create_service_linked_role_spot = true
   aws_region                      = local.aws_region
   vpc_id                          = module.vpc.vpc_id
