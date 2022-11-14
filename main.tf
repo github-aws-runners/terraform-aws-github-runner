88a3d17c35a468bca86d62f8e4f68e2e6506bb8a
navdeepg2021 <navdeepg2021@gmail.com>
2022-11-14 16:36:09 +0530

    fix: variable name.

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_sqs_queue_policy" "build_queue_dlq_policy" {
 
 resource "aws_sqs_queue" "queued_builds_dlq" {
   count = var.redrive_build_queue.enabled ? 1 : 0
-  name  = "${var.prefix}-queued-builds_dead_letter${each.value.fifo ? ".fifo" : ""}"
+  name  = "${var.prefix}-queued-builds_dead_letter${var.fifo_build_queue ? ".fifo" : ""}"
 
   sqs_managed_sse_enabled           = var.queue_encryption.sqs_managed_sse_enabled
   kms_master_key_id                 = var.queue_encryption.kms_master_key_id
