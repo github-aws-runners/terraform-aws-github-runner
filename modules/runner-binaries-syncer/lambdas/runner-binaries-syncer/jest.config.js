6c08e60a5f0f8f5dfb16e0d4e9e417783b1fffce
Niek Palm <niek.palm@philips.com>
2023-03-22 11:30:58 +0100

    increase coverage thresholds

--- modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/jest.config.js
+++ modules/runner-binaries-syncer/lambdas/runner-binaries-syncer/jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,js,jsx}', '!src/**/*local*.ts', '!src/**/*.d.ts'],
   coverageThreshold: {
     global: {
-      branches: 86,
-      functions: 90,
+      branches: 87,
+      functions: 92,
       lines: 98,
       statements: 98
     }
