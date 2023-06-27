3a72a4bbdc99eaca5b82178c3146c37758af06b9
strophy <badfunkstripe@gmail.com>
2023-06-27 16:10:52 +1000

    chore: increase runner storage to 70GB

--- examples/platform/main.tf
+++ examples/platform/main.tf
@@ -47,7 +47,7 @@ module "runners" {
     device_name           = "/dev/sda1"
     delete_on_termination = true
     volume_type           = "gp3"
-    volume_size           = 50
+    volume_size           = 70
     encrypted             = true
     iops                  = null
     throughput            = null
