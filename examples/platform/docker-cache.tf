80262083038e21c239f2837715a05bf14a94a8d4
strophy <badfunkstripe@gmail.com>
2023-07-06 16:47:39 +1000

    feat: set up ASG and ALB in front of docker cache

--- examples/platform/docker-cache.tf
+++ examples/platform/docker-cache.tf
@@ -22,7 +22,7 @@ data "aws_security_group" "runner_sg" {
 
 resource "aws_security_group" "docker_cache_sg" {
   name_prefix = "${local.environment}-docker-cache-sg"
-  vpc_id = module.base.vpc.vpc_id
+  vpc_id      = module.base.vpc.vpc_id
   tags = {
     Name = "docker-cache-sg"
   }
@@ -30,20 +30,18 @@ resource "aws_security_group" "docker_cache_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.docker_cache_sg.id
-  # description =
-  cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port   = 22
-  to_port     = 22
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "docker" {
-  security_group_id = aws_security_group.docker_cache_sg.id
-  # description =
+  security_group_id            = aws_security_group.docker_cache_sg.id
   referenced_security_group_id = data.aws_security_group.runner_sg.id
-  ip_protocol = "tcp"
-  from_port   = 443
-  to_port     = 443
+  ip_protocol                  = "tcp"
+  from_port                    = 5000
+  to_port                      = 5000
 }
 
 resource "aws_route53_zone" "private" {
@@ -58,14 +56,15 @@ resource "aws_route53_record" "docker_cache" {
   name    = "docker-cache.platform.internal"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.docker_cache.private_ip]
+  records = [aws_lb.docker_cache.dns_name]
 }
 
-resource "aws_instance" "docker_cache" {
-  ami = data.aws_ami.docker_cache_ami.id
+resource "aws_launch_template" "docker_cache" {
+  image_id      = data.aws_ami.docker_cache_ami.id
   instance_type = "t4g.micro"
+  name_prefix   = "${local.environment}-docker-cache"
+}
 
-  subnet_id = module.base.vpc.private_subnets[0]
   vpc_security_group_ids = [
     data.aws_security_group.runner_sg.id,
     aws_security_group.docker_cache_sg.id
@@ -74,31 +73,69 @@ resource "aws_instance" "docker_cache" {
   # TODO: Implement SSM
   # iam_instance_profile =
 
-  # Uncomment for debug access
-  # key_name = "dashdev.rsa"
-  # associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              apt-get update -y
+              apt-get install -y docker-ce docker-ce-cli containerd.io
+              usermod -aG ubuntu docker
+              echo -e "---\n\nversion: 0.1\nlog:\n  level: info\n  fields:\n    service: registry\nstorage:\n  cache:\n    blobdescriptor: inmemory\n  filesystem:\n    rootdirectory: /var/lib/registry\nhttp:\n  addr: :5000\n  headers:\n    X-Content-Type-Options: [nosniff]\nproxy:\n  remoteurl: https://registry-1.docker.io" > /home/ubuntu/config.yml
+              mkdir /home/ubuntu/registry
+              docker run -d -p 5000:5000 --restart=always --name=through-cache -v /home/ubuntu/config.yml:/etc/docker/registry/config.yml -v /home/ubuntu/registry:/var/lib/registry registry:2
+              EOF
 
-  user_data_replace_on_change = true
-  user_data                   = <<-EOF
-                                #!/bin/bash
-                                apt-get update -y
-                                apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-                                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-                                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                                apt-get update -y
-                                apt-get install -y docker-ce docker-ce-cli containerd.io
-                                usermod -aG ubuntu docker
-                                echo -e "---\n\nversion: 0.1\nlog:\n  level: info\n  fields:\n    service: registry\nstorage:\n  cache:\n    blobdescriptor: inmemory\n  filesystem:\n    rootdirectory: /var/lib/registry\nhttp:\n  addr: :5000\n  headers:\n    X-Content-Type-Options: [nosniff]\nproxy:\n  remoteurl: https://registry-1.docker.io" > /home/ubuntu/config.yml
-                                mkdir /home/ubuntu/registry
-                                docker run -d -p 443:5000 --restart=always --name=through-cache -v /home/ubuntu/config.yml:/etc/docker/registry/config.yml -v /home/ubuntu/registry:/var/lib/registry registry:2
-                                EOF
-
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 20
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 50
+      volume_type = "gp3"
   }
 
   tags = {
     Name = "platform-docker-cache-tf"
   }
+}
+
+resource "aws_autoscaling_group" "docker_cache" {
+  name_prefix         = "${local.environment}-docker-cache"
+  vpc_zone_identifier = module.base.vpc.private_subnets
+  launch_template {
+    id      = aws_launch_template.docker_cache.id
+    version = "$Latest"
+  }
+  min_size                  = 1
+  max_size                  = 2
+  desired_capacity          = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+
+  target_group_arns = [aws_lb_target_group.docker_cache.arn]
+  tag {
+    key                 = "Name"
+    value               = "platform-docker-cache-tf"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "docker_cache" {
+  name                       = "platform-docker-cache-tf"
+  internal                   = true
+  load_balancer_type         = "application"
+  subnets                    = module.base.vpc.private_subnets
+  security_groups            = [aws_security_group.docker_cache_sg.id]
+  enable_deletion_protection = true
+  tags = {
+    Name = "platform-docker-cache-tf"
+  }
+}
+
+resource "aws_lb_target_group" "docker_cache" {
+  name        = "platform-docker-cache-tf"
+  target_type = "alb"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = module.base.vpc.vpc_id
 }
