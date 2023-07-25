6c1c548b5e587283b08ba61b319e7d78f8ecb332
strophy <badfunkstripe@gmail.com>
2023-07-25 13:50:22 +1000

    chore: remove old s3 cache module invocation

--- modules/multi-runner/s3-cache.tf
+++
@@ -1,9 +0,0 @@
-# module "s3-cache" {
-#   source   = "../s3-cache"
-#   for_each = local.runner_config
-#   # prefix   = "${var.prefix}-${each.key}"
-#   bucket_name = "${var.prefix}-${each.key}"
-#   tags = merge(local.tags, {
-#     "ghr:environment" = "${var.prefix}-${each.key}"
-#   })
-# }
