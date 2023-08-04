99779e2e42bacf393118d5de01cd7f2f3ca78352
strophy <badfunkstripe@gmail.com>
2023-08-04 14:50:46 +1000

    chore: remove redundant security group rule

--- modules/multi-runner/docker_cache/main.tf
+++ modules/multi-runner/docker_cache/main.tf
@@ -20,14 +20,6 @@ resource "aws_security_group" "docker_cache_sg" {
   tags        = var.config.tags
 }
 
-resource "aws_vpc_security_group_ingress_rule" "docker" {
-  security_group_id            = aws_security_group.docker_cache_sg.id
-  referenced_security_group_id = aws_security_group.docker_cache_sg.id
-  ip_protocol                  = "tcp"
-  from_port                    = 5000
-  to_port                      = 5000
-}
-
 resource "aws_vpc_security_group_egress_rule" "docker" {
   security_group_id = aws_security_group.docker_cache_sg.id
   ip_protocol       = "-1"
