8afd0b49be593f4038cc61aea3e00660eb55a571
ktechmidas <daniel@ktechmidas.net>
2023-09-28 19:47:51 +0000

    fix: ephemeral storage not mounting correctly

--- images/ubuntu-jammy-platform/start-runner.sh
+++ images/ubuntu-jammy-platform/start-runner.sh
@@ -4,8 +4,9 @@ exec > >(tee /var/log/runner-startup.log | logger -t user-data -s 2>/dev/console
 echo "Initializing NVMe Instance Store for Docker"
 sudo systemctl stop docker
 sudo mkdir -p /mnt/instance-store
-sudo mkfs -t xfs /dev/nvme1n1
-sudo mount /dev/nvme1n1 /mnt/instance-store
+EPHEMERAL_DISK=$(sudo nvme list | grep 'Amazon EC2 NVMe Instance Storage' | awk '{ print $1 }')
+sudo mkfs -t xfs $EPHEMERAL_DISK
+sudo mount $EPHEMERAL_DISK /mnt/instance-store
 sudo touch /etc/docker/daemon.json
 sudo sed -i '1s/{$/{\n  "data-root": "\/mnt\/instance-store",/' /etc/docker/daemon.json
 sudo systemctl start docker
