310e1cbefcb46a84270a66cf33e005b8535072a7
Niek Palm <niek.palm@philips.com>
2023-10-26 13:40:25 +0200

    upgrade lambda dependencies

--- lambdas/functions/gh-agent-syncer/src/syncer/syncer.ts
+++ lambdas/functions/gh-agent-syncer/src/syncer/syncer.ts
@@ -1,4 +1,4 @@
-import { GetObjectTaggingCommand, S3Client, Tag } from '@aws-sdk/client-s3';
+import { GetObjectTaggingCommand, S3Client, ServerSideEncryption, Tag } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
 import { Octokit } from '@octokit/rest';
 import { createChildLogger } from '@terraform-aws-github-runner/aws-powertools-util';
@@ -71,7 +71,7 @@ async function uploadToS3(
       Key: cacheObject.key,
       Tagging: versionKey + '=' + actionRunnerReleaseAsset.name,
       Body: passThrough,
-      ServerSideEncryption: process.env.S3_SSE_ALGORITHM,
+      ServerSideEncryption: process.env.S3_SSE_ALGORITHM as ServerSideEncryption,
     },
   });
 
