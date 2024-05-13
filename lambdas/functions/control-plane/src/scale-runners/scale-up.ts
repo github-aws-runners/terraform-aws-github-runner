0abb2ff497a9b3c7e46717a022903f8fecf0e7be
Pedro Goncalves <pedro.goncalves@schibsted.com>
2024-05-13 23:45:19 +0200

    bug(lambda): Prevent scale-up lambda from starting runner for user repo if org level runners is enabled

--- lambdas/functions/control-plane/src/scale-runners/scale-up.ts
+++ lambdas/functions/control-plane/src/scale-runners/scale-up.ts
@@ -252,14 +252,7 @@ export async function scaleUp(eventSource: string, payload: ActionRequestMessage
     );
   }
 
-  if (enableOrgLevel && payload.repoOwnerType !== 'Organization') {
-    logger.warn(`Repository ${payload.repositoryOwner}/${payload.repositoryName} does not belong to a GitHub` +
-    `organization and organization runners are enabled. This is not supported. Not scaling up for this event.`);
-    throw Error(
-      `Repository ${payload.repositoryOwner}/${payload.repositoryName} does not belong to a GitHub` +
-    `organization and organization runners are enabled. This is not supported. Not scaling up for this event.`,
-    );
-  }
+  validateRepoOwnerTypeIfOrgLevelEnabled(payload, enableOrgLevel);
 
   const ephemeral = ephemeralEnabled && payload.eventType === 'workflow_job';
   const runnerType = enableOrgLevel ? 'Org' : 'Repo';
@@ -350,6 +343,18 @@ async function createStartRunnerConfig(
   } else {
     await createRegistrationTokenConfig(githubRunnerConfig, instances, ghClient);
   }
+}
+
+function validateRepoOwnerTypeIfOrgLevelEnabled(payload: ActionRequestMessage, enableOrgLevel: boolean) {
+  if (enableOrgLevel && payload.repoOwnerType !== 'Organization') {
+    logger.warn(`Repository ${payload.repositoryOwner}/${payload.repositoryName} does not belong to a GitHub` +
+      `organization and organization runners are enabled. This is not supported. Not scaling up for this event.`);
+    throw Error(
+      `Repository ${payload.repositoryOwner}/${payload.repositoryName} does not belong to a GitHub` +
+      `organization and organization runners are enabled. This is not supported. Not scaling up for this event.`,
+    );
+  }
+
 }
 
 function addDelay(instances: string[]) {
