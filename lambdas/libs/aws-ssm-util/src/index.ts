b6eaf01fd2da52f8499a5232d2aabdf2062abc08
samchungy <samchungy@gmail.com>
2024-06-10 12:32:41 +1000

    Reuse Clients

--- lambdas/libs/aws-ssm-util/src/index.ts
+++ lambdas/libs/aws-ssm-util/src/index.ts
@@ -1,8 +1,10 @@
 import { GetParameterCommand, PutParameterCommand, SSMClient, Tag } from '@aws-sdk/client-ssm';
 import { getTracedAWSV3Client } from '@terraform-aws-github-runner/aws-powertools-util';
 
+let ssmClient: SSMClient;
+
 export async function getParameter(parameter_name: string): Promise<string> {
-  const client = getTracedAWSV3Client(new SSMClient({ region: process.env.AWS_REGION }));
+  const client = getTracedAWSV3Client((ssmClient ??= new SSMClient({ region: process.env.AWS_REGION })));
   return (await client.send(new GetParameterCommand({ Name: parameter_name, WithDecryption: true }))).Parameter
     ?.Value as string;
 }
@@ -13,7 +15,7 @@ export async function putParameter(
   secure: boolean,
   options: { tags?: Tag[] } = {},
 ): Promise<void> {
-  const client = getTracedAWSV3Client(new SSMClient({ region: process.env.AWS_REGION }));
+  const client = getTracedAWSV3Client((ssmClient ??= new SSMClient({ region: process.env.AWS_REGION })));
   await client.send(
     new PutParameterCommand({
       Name: parameter_name,
