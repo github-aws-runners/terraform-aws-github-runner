ddae611b9974c1f58b6e3aa101dd453e1dd309a3
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-10-08 16:35:10 +0200

    chore: fix build errors

--- lambdas/functions/termination-watcher/src/lambda.test.ts
+++ lambdas/functions/termination-watcher/src/lambda.test.ts
@@ -55,7 +55,7 @@ describe('Handle sport termination interruption warning', () => {
         resolve();
       });
     });
-    expect(await interruptionWarning(event, context)).resolves;
+    await expect(interruptionWarning(event, context)).resolves.toBeUndefined();
   });
 
   it('should not throw only log in error in case of an exception.', async () => {
