8540f71c5063ebf6ab604caa50e9ff0711f91255
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-10-09 09:02:05 +0200

    Update lambdas/functions/ami-housekeeper/src/lambda.test.ts
    Co-authored-by: Niek Palm <npalm@users.noreply.github.com>

--- lambdas/functions/ami-housekeeper/src/lambda.test.ts
+++ lambdas/functions/ami-housekeeper/src/lambda.test.ts
@@ -52,7 +52,7 @@ describe('Housekeeper ami', () => {
         resolve();
       });
     });
-    await expect(handler(undefined, context)).resolves.toBeUndefined();
+    await expect(handler(undefined, context)).resolves.not.toThrow();
   });
 
   it('should not thow only log in error in case of an exception.', async () => {
