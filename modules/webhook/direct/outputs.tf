0c8e93f01d23954e36c57ab9878efbb14fb94dc7
Niek Palm <niek.palm@philips.com>
2024-10-21 12:35:27 +0200

    remove commented code

--- modules/webhook/direct/outputs.tf
+++ modules/webhook/direct/outputs.tf
@@ -10,25 +10,3 @@ output "webhook" {
     role      = aws_iam_role.webhook_lambda
   }
 }
-
-# output "lambda_log_group" {
-#   value = aws_cloudwatch_log_group.webhook
-# }
-
-# output "role" {
-#   value = aws_iam_role.webhook_lambda
-# }
-
-# output "endpoint_relative_path" {
-#   value = local.webhook_endpoint
-# }
-
-
-
-# output "webhook_lambda_function_name" {
-#   value = module.webhook.lambda_function_name
-# }
-
-# output "webhook_cloudwatch_log_group_name" {
-#   value = module.webhook.cloudwatch_log_group_name
-# }
