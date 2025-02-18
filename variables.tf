64bda92fa436dcfcca031608c81359bef74dce95
AppliNH <applinh@protonmail.com>
2025-02-18 17:03:07 +0100

    other(vars github_app): add note regarding precedence

--- variables.tf
+++ variables.tf
@@ -37,6 +37,7 @@ variable "github_app" {
   You can optionally create the SSM parameters yourself and provide the ARN and name here, through the `*_ssm` attributes.
   If you chose to provide the configuration values directly here, 
   please ensure the key is the base64-encoded `.pem` file (the output of `base64 app.private-key.pem`, not the content of `private-key.pem`).
+  Note: the provided SSM parameters arn and name have a precedence over the actual value (i.e `key_base64_ssm` has a precedence over `key_base64` etc).
   EOF
   type = object({
     key_base64 = optional(string)
