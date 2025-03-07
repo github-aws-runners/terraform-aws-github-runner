50be5415f6f16d0968c7c8f9a934dd9d2e00ffe2
Niek Palm <niek.palm@philips.com>
2025-03-07 13:09:49 +0100

    clean up
    - add vitest vscode plugin suggestion
    - remove jest stuff

--- lambdas/libs/aws-ssm-util/vitest.config.ts
+++ lambdas/libs/aws-ssm-util/vitest.config.ts
@@ -3,7 +3,7 @@ import defaultConfig from '../../vitest.base.config';
 
 export default mergeConfig(defaultConfig, {
   test: {
-    setupFiles: ['./aws-vitest-setup.ts'],
+    setupFiles: ['../../aws-vitest-setup.ts'],
     coverage: {
       include: ['src/**/*.ts'],
       exclude: ['src/**/*.test.ts', 'src/**/*.d.ts'],
