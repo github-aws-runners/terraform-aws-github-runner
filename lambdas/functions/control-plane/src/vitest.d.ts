d529c1240eb5b464a446f8347a9836711f6194df
Niek Palm <niek.palm@philips.com>
2025-03-17 13:48:15 +0100

    clean up

--- lambdas/functions/control-plane/src/vitest.d.ts
+++
@@ -1,7 +0,0 @@
-// import 'vitest';
-// import { CustomMatcher } from 'aws-sdk-client-mock-jest';
-
-// declare module 'vitest' {
-//   type CustomAssertion<T = unknown> = CustomMatcher<T>;
-//   type CustomAsymmetricMatchersContaining = CustomMatcher;
-// }
