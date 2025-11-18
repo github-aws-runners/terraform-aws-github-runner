2cb2a8735d50613f63dd6e8c689ea466b02ccf02
Jon Anderson <1361123+jonanderson10@users.noreply.github.com>
2025-11-18 14:52:29 -0800

    fix: use pwsh to download gh runner

--- images/windows-core-2025/windows-provisioner.ps1
+++ images/windows-core-2025/windows-provisioner.ps1
@@ -30,14 +30,22 @@ Remove-Item C:\amazon-cloudwatch-agent.msi
 
 # Install dependent tools
 Write-Host "Installing additional development tools"
-choco install git awscli -y
+choco install git awscli powershell-core -y
 refreshenv
 
 Write-Host "Creating actions-runner directory for the GH Action installation"
 New-Item -ItemType Directory -Path C:\actions-runner ; Set-Location C:\actions-runner
 
 Write-Host "Downloading the GH Action runner from ${action_runner_url}"
-Invoke-WebRequest -Uri ${action_runner_url} -OutFile actions-runner.zip
+# Direct download with WebClient to prevent forced disconnects
+$downloadCommand = @"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+`$webClient = New-Object System.Net.WebClient
+`$webClient.Headers.Add('User-Agent', 'PowerShell/7.0')
+`$webClient.DownloadFile('${action_runner_url}', 'actions-runner.zip')
+`$webClient.Dispose()
+"@
+pwsh -Command $downloadCommand
 
 Write-Host "Un-zip action runner"
 Expand-Archive -Path actions-runner.zip -DestinationPath .
