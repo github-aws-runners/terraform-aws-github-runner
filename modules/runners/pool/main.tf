b02518ce4920041468e4783ae4d981987c6179ad
Alex Albu <alex.albu@bloomandwild.com>
2025-12-12 14:55:55 +0000

    feat: use prefix variable for POWERTOOLS_SERVICE_NAME in Lambda functions

--- modules/runners/pool/main.tf
+++ modules/runners/pool/main.tf
@@ -42,7 +42,7 @@ resource "aws_lambda_function" "pool" {
       SSM_TOKEN_PATH                           = var.config.ssm_token_path
       SSM_CONFIG_PATH                          = var.config.ssm_config_path
       SUBNET_IDS                               = join(",", var.config.subnet_ids)
-      POWERTOOLS_SERVICE_NAME                  = "runners-pool"
+      POWERTOOLS_SERVICE_NAME                  = "${var.config.prefix}-pool"
       POWERTOOLS_TRACE_ENABLED                 = var.tracing_config.mode != null ? true : false
       POWERTOOLS_TRACER_CAPTURE_HTTPS_REQUESTS = var.tracing_config.capture_http_requests
       POWERTOOLS_TRACER_CAPTURE_ERROR          = var.tracing_config.capture_error
