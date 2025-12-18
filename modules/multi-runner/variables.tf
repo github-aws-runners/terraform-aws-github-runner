6bfe6481bbb4aee772d2bf0bb4dbd212d4683a28
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-12-18 21:51:42 +0100

    feat: add license_specification

--- modules/multi-runner/variables.tf
+++ modules/multi-runner/variables.tf
@@ -147,6 +147,9 @@ variable "multi_runner_config" {
         tenancy                 = optional(string)
         partition_number        = optional(number)
       }), null)
+      license_specification = optional(object({
+        license_configuration_arn = string
+      }), null)
       runner_log_files = optional(list(object({
         log_group_name   = string
         prefix_log_group = bool
