e142885435f2aede6774520308a27077ab9b11a3
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-01-07 11:50:23 +0100

    fix: undo windows change

--- modules/runners/templates/install-runner.ps1
+++ modules/runners/templates/install-runner.ps1
@@ -1,31 +1,13 @@
 ## install the runner
 
 Write-Host "Creating actions-runner directory for the GH Action installation"
-New-Item -ItemType Directory -Path C:\actions-runner -Force | Out-Null
-Set-Location C:\actions-runner
+New-Item -ItemType Directory -Path C:\actions-runner ; Set-Location C:\actions-runner
 
-$runnerTarballUrl = $Env:RUNNER_TARBALL_URL
-$s3RunnerDistribution = $Env:S3_LOCATION_RUNNER_DISTRIBUTION
-
-if ([string]::IsNullOrWhiteSpace($runnerTarballUrl) -and [string]::IsNullOrWhiteSpace($s3RunnerDistribution)) {
-  Write-Host "Neither RUNNER_TARBALL_URL nor S3_LOCATION_RUNNER_DISTRIBUTION are set" -ForegroundColor Red
-  exit 1
-}
-
-$fileName = "actions-runner.zip"
-
-if (-not [string]::IsNullOrWhiteSpace($runnerTarballUrl)) {
-  Write-Host "Downloading the GH Action runner from $runnerTarballUrl to $fileName"
-  Invoke-WebRequest -Uri $runnerTarballUrl -OutFile $fileName -UseBasicParsing
-}
-else {
-  Write-Host "Downloading the GH Action runner from s3 bucket $s3RunnerDistribution"
-  aws s3 cp $s3RunnerDistribution $fileName | Out-Null
-}
+Write-Host "Downloading the GH Action runner from s3 bucket $s3_location"
+aws s3 cp ${S3_LOCATION_RUNNER_DISTRIBUTION} actions-runner.zip
 
 Write-Host "Un-zip action runner"
 Expand-Archive -Path actions-runner.zip -DestinationPath .
 
 Write-Host "Delete zip file"
 Remove-Item actions-runner.zip
-
