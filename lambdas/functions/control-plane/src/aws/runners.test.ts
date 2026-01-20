27fbfc0596d25094b6e63626c21b6ea9b86a152d
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-01-20 14:33:51 +0100

    test: fix test cases

--- lambdas/functions/control-plane/src/aws/runners.test.ts
+++ lambdas/functions/control-plane/src/aws/runners.test.ts
@@ -319,7 +319,7 @@ describe('create runner', () => {
     allocationStrategy: SpotAllocationStrategy.CAPACITY_OPTIMIZED,
     capacityType: 'spot',
     type: 'Org',
-    scaleErrors: [],
+    scaleErrors: ['UnfulfillableCapacity', 'MaxSpotInstanceCountExceeded'],
     source: 'scale-up-lambda',
   };
 
@@ -802,7 +802,7 @@ describe('create runner with errors fail over to OnDemand', () => {
     capacityType: 'spot',
     type: 'Repo',
     onDemandFailoverOnError: ['InsufficientInstanceCapacity'],
-    scaleErrors: [],
+    scaleErrors: ['UnfulfillableCapacity', 'MaxSpotInstanceCountExceeded'],
     source: 'scale-up-lambda',
   };
   const defaultExpectedFleetRequestValues: ExpectedFleetRequestValues = {
