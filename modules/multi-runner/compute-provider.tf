70e2a4bb0e14fcd4d0e2580601957d3673a27c52
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-08-08 02:08:12 +0200

    refactor(multi-runner): simplify provider type selection

--- modules/multi-runner/compute-provider.tf
+++ modules/multi-runner/compute-provider.tf
@@ -1,14 +1,9 @@
 locals {
-  compute_provider_type_candidates = {
-    for runner_key, runner_config in local.multi_runner_config : runner_key => [
+  compute_provider_types = {
+    for runner_key, runner_config in local.multi_runner_config : runner_key => one([
       for provider_type, provider_config in runner_config.compute_provider : provider_type
       if provider_config != null
-    ]
-  }
-
-  compute_provider_types = {
-    for runner_key, provider_types in local.compute_provider_type_candidates :
-    runner_key => one(provider_types)
+    ])
   }
 
   runner_config_by_provider = {
