c2a85663344a48a1236961636e553ef7e9d79faa
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-08-19 10:15:33 +0200

    refactor(storage): extract GitHub App credentials

--- lambdas/libs/storage-providers/aws/ssm/environment.d.ts
+++ lambdas/libs/storage-providers/aws/ssm/environment.d.ts
@@ -3,6 +3,9 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      PARAMETER_GITHUB_APP_ID_NAME?: string;
+      PARAMETER_GITHUB_APP_INSTALLATION_ID_NAME?: string;
+      PARAMETER_GITHUB_APP_KEY_BASE64_NAME?: string;
       SSM_CONFIG_PATH?: string;
       SSM_CLEANUP_CONFIG?: string;
       SSM_PARAMETER_STORE_TAGS?: string;
