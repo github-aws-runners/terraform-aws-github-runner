70c57558fc79780b7148eae8aad159140012da21
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-09-03 20:31:20 +0200

    fix(multi-runner): use shared webhook tags

--- modules/multi-runner/webhook.tf
+++ modules/multi-runner/webhook.tf
@@ -24,7 +24,7 @@ locals {
 module "webhook" {
   source      = "../webhook"
   prefix      = var.prefix
-  tags        = local.effective_config.tags
+  tags        = local.tags
   kms_key_arn = local.effective_config.ssm.kms_key_id
   eventbridge = {
     enable        = local.effective_config.orchestration_provider.webhook.eventbridge.enabled
