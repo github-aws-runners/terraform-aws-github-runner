9c80bf88e3f24a71fee8dfad958914d3c99e2b91
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-09-11 14:02:13 +0200

    fix(multi-runner): wire global scale-set configuration

--- modules/multi-runner/orchestration-provider.scale-set.tf
+++ modules/multi-runner/orchestration-provider.scale-set.tf
@@ -8,7 +8,8 @@ locals {
   scale_set_runner_configs = {
     for runner_name, runner_config in local.scale_set_runner_config : runner_name => {
       github = {
-        config_url = runner_config.orchestration_provider.scale_set.github.config_url
+        config_url        = local.effective_config.github.config_url
+        enterprise_server = local.effective_config.github.enterprise_server
         app = {
           app_id = {
             name        = local.primary_app_id.name
@@ -21,28 +22,21 @@ locals {
             kms_key_arn = local.effective_config.ssm.kms_key_id
           }
           installation_id = {
-            name        = runner_config.orchestration_provider.scale_set.github.installation_id_ssm.name
-            arn         = runner_config.orchestration_provider.scale_set.github.installation_id_ssm.arn
-            kms_key_arn = runner_config.orchestration_provider.scale_set.github.installation_id_ssm.kms_key_arn
+            name        = local.primary_app_installation_id.name
+            arn         = local.primary_app_installation_id.arn
+            kms_key_arn = local.effective_config.ssm.kms_key_id
           }
         }
-        force_ghes = try(coalesce(
-          runner_config.orchestration_provider.scale_set.github.force_ghes,
-          local.effective_config.github.enterprise_server.url != null,
-        ), false)
-        ssl_verify = local.effective_config.github.enterprise_server.ssl_verify
         user_agent = local.effective_config.github.user_agent
       }
       scale_set = {
-        name                 = runner_config.orchestration_provider.scale_set.name
-        id                   = runner_config.orchestration_provider.scale_set.id
-        runner_group_id      = runner_config.orchestration_provider.scale_set.runner_group_id
-        min_runners          = runner_config.orchestration_provider.scale_set.min_runners
-        max_runners          = runner_config.orchestration_provider.scale_set.max_runners
-        boot_time_in_minutes = runner_config.orchestration_provider.scale_set.boot_time_in_minutes
-        session_owner        = runner_config.orchestration_provider.scale_set.session_owner
+        name = runner_config.orchestration_provider.scale_set.name
+        runner = {
+          min_runners          = runner_config.orchestration_provider.scale_set.runner.min_runners
+          max_runners          = runner_config.orchestration_provider.scale_set.runner.max_runners
+          boot_time_in_minutes = runner_config.orchestration_provider.scale_set.runner.boot_time_in_minutes
+        }
       }
-      work_folder = runner_config.orchestration_provider.scale_set.work_folder
     }
   }
 
