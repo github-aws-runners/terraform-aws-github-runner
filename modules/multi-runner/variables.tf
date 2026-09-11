b8a19684eed956f35ce02c42369599b58455d0b3
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-09-11 15:58:25 +0200

    fix(multi-runner): default v2 experimental feature

--- modules/multi-runner/variables.tf
+++ modules/multi-runner/variables.tf
@@ -80,7 +80,7 @@ variable "experimental_features" {
     for one release when the feature graduates.
   EOT
   type        = set(string)
-  default     = []
+  default     = ["multi-runner-v2"]
 
   validation {
     condition     = alltrue([for feature in var.experimental_features : feature == "multi-runner-v2"])
