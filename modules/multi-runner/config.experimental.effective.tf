877182749a1f2824abe80e1c20a4bb4f3373fa90
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-09-11 14:32:29 +0200

    fix(multi-runner): use direct effective scale-set value

--- modules/multi-runner/config.experimental.effective.tf
+++ modules/multi-runner/config.experimental.effective.tf
@@ -35,10 +35,7 @@ locals {
               artifact = local.normalized_config.orchestration_provider.webhook.lambda.artifact
             })
           })
-          scale_set = v.orchestration_provider.scale_set == null ? null : try(coalesce(
-            v.orchestration_provider.scale_set,
-            local.normalized_config.orchestration_provider.scale_set,
-          ), null)
+          scale_set = v.orchestration_provider.scale_set
         }
 
         ssm = merge(v.ssm, {
